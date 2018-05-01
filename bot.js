22153da15907923633366e9e4a3791e027706daf
Mehullll <38818168+Mehullll@users.noreply.github.com>
2018-05-01 16:08:54 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,20 @@ const fs = require("fs");
 const client = new Discord.Client();
 const images = JSON.parse(fs.readFileSync("./pokemonrefs.json", "utf8"));
 
+//var myList = JSON.parse(fs.readFileSync("./list.json", "utf8"));
 var interval;
 var spamid = [];
 var infoid = [];
 var curr = 0;
 var testchannel = "436971996736258049";
+
+var listado = ["mew", "mewtwo", "rayquaza", "moltres", "articuno", "celebi", "zenaora", "necrozma", "poipole", 
+    "marshadow", "zygarde", "lugia", "cosmog", "kyurem", "solgaleo", "latios", "latias", "silvally", "giratina", 
+    "lunala", "kyogre", "stakataka", "dialga", "cosmoem", "suicune", "tapu koko", "zekrom", "zapdos", "regigigas", 
+    "palkia", "reshiram", "xerneas", "entei", "nihilego", "yveltal", "heatran", "guzzlord", "kartana", "pheromosa", 
+    "raikou", "landorus", "cresselia", "thundurus", "tapu lele", "tornadus", "cobalion", "regirock", "tapu fini", 
+    "xurkitree", "tapu bulu", "regice", "registeel", "virizion", "mesprit", "azelf", "uxie", "terrakion",
+    "metang", "metagross"];
 
 /*function step() {
     if (spamid.length > 0) {
@@ -29,6 +38,18 @@ client.on('message', message => {
     if (message.content === '$ping') {
     	message.reply('pong');
     }
+    
+    /*if (message.content === '$reset') { 
+        fs.unlinkSync("./list.json");
+        fs.writeFile("./list.json", JSON.stringify(listado), function (err) {
+            if (err) {
+                message.channel.send('Error writting file');
+            } else {
+                message.channel.send('File saved: ' + listado);
+                myList = listado;
+            }
+        });       
+    }*/
     
     if (message.content === '$spam') { 
         if (spamid.indexOf(message.channel.id) < 0) {
@@ -85,7 +106,46 @@ client.on('message', message => {
     if (message.content === '$infochannels') { 
         message.channel.send('spawns info channels: ' + infoid.join(' '));
     }
+  
+    if (message.content === '$list') { 
+        message.channel.send('Pokemon to catch: \n' + listado.join(' '));
+    }
 
+    if (message.content.includes('$add') && message.content.split(' ').length > 1) { 
+        var toadd = message.content.replace('$add ','');
+        var index = listado.indexOf(toadd);
+        if (index > -1) {
+            listado.splice(index, 1);
+        }
+        listado.push(toadd);
+        /*fs.unlinkSync("./list.json");
+        fs.writeFile("./list.json", JSON.stringify(myList), function (err) {
+            if (err) {
+                message.channel.send('Error adding pokemon');
+            } else {
+                message.channel.send('Added pokemon to list: ' + toadd);
+            }
+        });*/
+        message.channel.send('Added pokemon to list: ' + toadd);
+    }
+
+    if (message.content.includes('$remove') && message.content.split(' ').length > 1) { 
+        var torem = message.content.replace('$remove ','');
+        var index = listado.indexOf(torem);
+        if (index > -1) {
+            listado.splice(index, 1);
+        }
+        /*fs.unlinkSync("./list.json");
+        fs.writeFile("./list.json", JSON.stringify(myList), function (err) {
+            if (err) {
+                message.channel.send('Error removing pokemon');
+            } else {
+                message.channel.send('Removed pokemon to list: ' + toadd);
+            }
+        });*/
+        message.channel.send('Removed pokemon to list: ' + toadd);
+    }
+    
     if (infoid.indexOf(message.channel.id) > -1) {
         if (message.embeds.length > 0) {
             emb = message.embeds[0];
@@ -94,9 +154,12 @@ client.on('message', message => {
                 name = emb.image.url.split('/').pop(-1).split('.')[0];
                 //message.channel.send(images[name]);
                 realname = images[name];
-                if (realname.length >0)
-                    //message.channel.send('A wild ' + realname + ' has appeared');
-                    message.channel.send('-catch ' + realname);
+                if (realname.length >0) {
+                    if (myList.indexOf(realname) > -1)
+                        message.channel.send('p!catch ' + realname);
+                    else
+                        message.channel.send('A wild ' + realname + ' has appeared');
+                }
             }
         }
     }
