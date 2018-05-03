14be3b1d68515f9cdd1d5c42478fa24f5b9e6680
Mehullll <38818168+Mehullll@users.noreply.github.com>
2018-05-03 10:05:12 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,7 +67,7 @@ client.on('message', message => {
             //client.channels.get(testchannel).send('spamming into ' + spamid[curr]);
             client.channels.get(spamid[curr]).send(str);
             curr++;
-        }, 2000);
+        }, 1000);
     }
     
     if (message.content === '$spam') { 
@@ -84,7 +84,7 @@ client.on('message', message => {
             //client.channels.get(testchannel).send('spamming into ' + spamid[curr]);
             client.channels.get(spamid[curr]).send('spamming here');
             curr++;
-        }, 2000);
+        }, 1000);
     }
     
     if (message.content === '$stop') {
@@ -103,7 +103,7 @@ client.on('message', message => {
                 //client.channels.get(testchannel).send('spamming into ' + spamid[curr]);
                 client.channels.get(spamid[curr]).send('spamming here');
                 curr++;
-            }, 2000);
+            }, 1000);
         }
     }
     
